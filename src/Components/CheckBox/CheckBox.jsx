9f50ec722e0c8e1b9d761209b5c08d00e8b2faaa
Miguel Ledesma Palacios <mledpal@gmail.com>
2023-10-02 13:56:43 +0200

    Cambios en interfaz

--- src/Components/CheckBox/CheckBox.jsx
+++ src/Components/CheckBox/CheckBox.jsx
@@ -1,8 +1,15 @@
 import "./CheckBox.css";
-export const CheckBox = () => {
+export const CheckBox = ({ enabled, setEnabled }) => {
 	return (
 		<label htmlFor='chkFilter'>
-			<input type='checkbox' name='chkFilter' />
+			<input
+				type='checkbox'
+				name='chkFilter'
+				onChange={() => {
+					setEnabled(!enabled);
+				}}
+				checked={enabled ? "checked" : ""}
+			/>
 		</label>
 	);
 };
--- src/Components/Filtro/Filtro.jsx
+++ src/Components/Filtro/Filtro.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { CheckBox, FechaSelector } from "..";
 import "./Filtro.css";
 
 export const Filtro = ({ obtenerDatos }) => {
+	const [enabled, setEnabled] = useState(true);
+
 	return (
 		<>
-			<CheckBox />
+			<CheckBox enabled={enabled} setEnabled={setEnabled} />
 			<FechaSelector tipo='Start' />
 			<FechaSelector tipo='Stop' />
+
 			<button
 				id='btnFetch'
 				type='button'
 				onClick={() => {
 					obtenerDatos();
 				}}
+				disabled={enabled ? "" : "disabled"}
 			>
 				Obtener
 			</button>
