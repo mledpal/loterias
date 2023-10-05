3b88da60f86972dd3bf2d2248ec088c5545f847e
Miguel Ledesma Palacios <mledpal@gmail.com>
2023-10-05 12:20:20 +0200

    Mejoras en visualización

--- src/Components/MostrarFiltrados/MostrarFiltrados.jsx
+++ src/Components/MostrarFiltrados/MostrarFiltrados.jsx
@@ -1,7 +1,12 @@
 import { Bola } from "../Bola/Bola";
 
 export const MostrarFiltrados = ({ numerosFiltrados }) => {
-	return numerosFiltrados.map((num) => {
-		return <Bola key={num} num={num} tipo='bola U' />;
-	});
+	return (
+		<div class='descartados'>
+			<h4>Números descartados</h4>
+			{numerosFiltrados.map((num) => {
+				return <Bola key={num} num={num} tipo='bola U' />;
+			})}
+		</div>
+	);
 };
